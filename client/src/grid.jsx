50fddf520da9f96456778e93e51d61c72979e6e8
Vincent <vincentquan64@gmail.com>
2020-03-13 16:49:18 -0700

    plant bombs in state

--- client/src/grid.jsx
+++ client/src/grid.jsx
@@ -2,16 +2,30 @@ import React, { useState } from "react";
 import Tile from "./tile.jsx";
 
 const Grid = ({row, col, bombs}) => {
-  const [grid, setGrid] = useState(null);
-  const [locations, setLocations] = useState(new Set());
-
   const Node = function(loc) {
     this.bomb = false;
     this.revealed = false;
     this.num = 0;
     this.loc = loc;
   }
-  const makeGrid = (row, col) => {
+  // const makeGrid = (row, col) => {
+  //   const newGrid = new Array(row).fill(null).map(
+  //     (el, rowInd) => (
+  //       (new Array(col).fill(null).map(
+  //         (el, colInd) => (
+  //           new Node({r: rowInd, c: colInd})
+  //         )
+  //       ))
+  //     )
+  //   )
+  //   return newGrid;
+  //   // if (!grid) {
+  //   //   setGrid(newGrid);
+  //   // }
+  // }
+
+  // const [grid, setGrid] = useState(null);
+  const [grid, setGrid] = useState(() => {
     const newGrid = new Array(row).fill(null).map(
       (el, rowInd) => (
         (new Array(col).fill(null).map(
@@ -21,11 +35,29 @@ const Grid = ({row, col, bombs}) => {
         ))
       )
     )
+    const locations = new Set();
+    ((num) => {
+      function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      } 
+      while (locations.size < num) {    
+        let coords = {
+          r: getRandomInt(row),
+          c: getRandomInt(col),
+        };
+        if (!locations.has(JSON.stringify(coords))) {
+          newGrid[coords.r][coords.c].bomb = true;
+          locations.add(JSON.stringify(coords));
+        }
+      }
+    })(bombs);
 
-    if (!grid) {
-      setGrid(newGrid);
-    }
-  }
+
+
+    return newGrid;
+  });
+  // const [locations, setLocations] = useState(new Set());
+
   
   const plantBombs = (num) => {
     function getRandomInt(max) {
@@ -43,7 +75,7 @@ const Grid = ({row, col, bombs}) => {
     }
   }
 
-  makeGrid(row, col);
+  // makeGrid(row, col);
   // plantBombs(bombs);
   console.log(grid);
 return <div></div>
